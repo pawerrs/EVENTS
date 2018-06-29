41c63aaa9609f506e89b19b5aa61b963b9d5c54f
Paweł Kowalik <pawerrs@gmail.com>
2018-06-29 15:36:56 +0200

    installed nugets

--- EVENTS_MOBILE_APP/EVENTS_MOBILE_APP/EVENTS_MOBILE_APP.Android/Resources/Resource.designer.cs
+++ EVENTS_MOBILE_APP/EVENTS_MOBILE_APP/EVENTS_MOBILE_APP.Android/Resources/Resource.designer.cs
@@ -26,6 +26,7 @@ namespace EVENTS_MOBILE_APP.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Splat.Resource.String.library_name = global::EVENTS_MOBILE_APP.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::EVENTS_MOBILE_APP.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3726,6 +3727,9 @@ namespace EVENTS_MOBILE_APP.Droid
 			
 			// aapt resource value: 0x7f090038
 			public const int character_counter_pattern = 2131296312;
+			
+			// aapt resource value: 0x7f09003e
+			public const int library_name = 2131296318;
 			
 			// aapt resource value: 0x7f090000
 			public const int mr_button_content_description = 2131296256;
